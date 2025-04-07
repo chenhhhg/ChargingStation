260d21daec3ff0be4fd33102ae478add9334c40b
“chenguanxian” <chenhhhh1717@gmail.com>
2025-04-07 13:53:00 +0800

    fix：解决部署后外界无法访问的bug

--- src/server.py
+++ src/server.py
@@ -11,4 +11,4 @@ async def root():
     return {"message": "hello world"}
 
 if __name__ == '__main__':
-    uvicorn.run(app="server:app", host="127.0.0.1", port=8080)
+    uvicorn.run(app="server:app", host="0.0.0.0", port=8080)
